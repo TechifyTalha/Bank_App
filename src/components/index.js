afa2927663698c9c58f401c3dbccfe2ddd764748
TechifyTalha <165843893+TechifyTalha@users.noreply.github.com>
2024-04-04 21:00:17 +0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import  FeedbackCard from './FeedbackCard'
+import GetStarted from "./GetStarted";
+import Clients from "./Clients";
+import CardDeal from "./CardDeal";
+import Button from './Button'
+
+export{
+Navbar,
+Clients,
+CTA,
+Business,
+Button,
+FeedbackCard,
+Hero,
+Testimonials,
+CardDeal,
+Stats,
+GetStarted,
+Footer,
+Billing,
+}
